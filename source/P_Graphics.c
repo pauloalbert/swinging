0d4c7ded76517ab53975041d08ff24447aed92b2
mathu974 <fmfromentmathurin@gmail.com>
2024-01-08 22:54:59 +0100

    cleaning

--- source/P_Graphics.c
+++ source/P_Graphics.c
@@ -183,12 +183,6 @@ void P_Graphics_setup_sub(){
 
 	P_Graphics_assignBuffer(SUB,BG_GFX_SUB+0x0000,256,192);
 
-	//swiCopy(PauseBitmap, BG_BMP_RAM_SUB(1), PauseBitmapLen/2);
-	//swiCopy(PausePal, BG_PALETTE_SUB, PausePalLen/2);
-
-	//swiCopy(GameOverBitmap, BG_BMP_RAM_SUB(5), GameOverBitmapLen/2);
-	//swiCopy(GameOverPal, &BG_PALETTE_SUB[PausePalLen], GameOverPalLen/2);
-
 	int i;
 	for(i = 0; i < 15; i++){
 		BG_PALETTE_SUB[i] = BG_PALETTE[i];
